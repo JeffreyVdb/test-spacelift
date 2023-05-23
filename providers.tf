8348bf5eb6694513e2f25abbab9bd58fc4f05040
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2023-05-23 23:06:54 +0200

    use assume role with web identity

--- providers.tf
+++ providers.tf
@@ -1,8 +1,9 @@
 provider "aws" {
   region = "eu-west-1"
 
-  assume_role {
-    role_arn = var.aws_assume_role_arn
+  assume_role_with_web_identity {
+    role_arn                = var.aws_assume_role_arn
+    web_identity_token_file = "/mnt/workspace/spacelift.oidc"
   }
 
   default_tags {
