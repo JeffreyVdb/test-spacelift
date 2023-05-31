ac0cb53b7e0318d1fe5ed621983b7a29ef92ad92
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2023-05-31 13:19:33 +0200

    use assume role instead of assume role with web identity

--- providers.tf
+++ providers.tf
@@ -1,9 +1,8 @@
 provider "aws" {
   region = "eu-west-1"
 
-  assume_role_with_web_identity {
-    role_arn                = var.aws_assume_role_arn
-    web_identity_token_file = "/mnt/workspace/spacelift.oidc"
+  assume_role {
+    role_arn = var.aws_assume_role_arn
   }
 
   default_tags {
